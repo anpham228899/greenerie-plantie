7cc6b2b72cbfd3cce0d13ff3833422760f0fc968
An Pham <anptt22411ca@st.uel.edu.vn>
2025-06-11 01:27:10 +0700

    added profile management

--- app/src/main/java/com/example/greenerieplantie/ProfileManagementActivity.java
+++ app/src/main/java/com/example/greenerieplantie/ProfileManagementActivity.java
@@ -1,6 +1,9 @@
 package com.example.greenerieplantie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,59 @@ public class ProfileManagementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile_management);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.layoutProfileSettings).setOnClickListener(v -> openProfileSettingsActivity());
+        findViewById(R.id.layoutOrderHistory).setOnClickListener(v -> openOrderHistoryActivity());
+        findViewById(R.id.layoutCareReminder).setOnClickListener(v -> openCareReminderActivity());
+        findViewById(R.id.layoutNotifications).setOnClickListener(v -> openNotificationsActivity());
+        findViewById(R.id.layoutChangeLanguage).setOnClickListener(v -> openChangeLanguageActivity());
+        findViewById(R.id.layoutAboutUs).setOnClickListener(v -> openAboutUsActivity());
+        findViewById(R.id.layoutPolicy).setOnClickListener(v -> openPolicyActivity());
+        findViewById(R.id.layoutCustomerSupport).setOnClickListener(v -> openCustomerSupportActivity());
     }
+        private void openProfileSettingsActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, ProfileSettingActivity.class);
+            startActivity(intent);
+        }
+
+        private void openOrderHistoryActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, OrderHistoryActivity.class);
+            startActivity(intent);
+        }
+
+        private void openCareReminderActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, CareReminderActivity.class);
+            startActivity(intent);
+        }
+
+        private void openNotificationsActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, NotificationActivity.class);
+            startActivity(intent);
+        }
+
+        private void openChangeLanguageActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, LanguageActivity.class);
+            startActivity(intent);
+        }
+
+        private void openAboutUsActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, AboutUsActivity.class);
+            startActivity(intent);
+        }
+
+        private void openPolicyActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, PoliciesActivity.class);
+            startActivity(intent);
+        }
+
+        private void openCustomerSupportActivity() {
+            Intent intent = new Intent(ProfileManagementActivity.this, CustomerSupportActivity.class);
+            startActivity(intent);
+        }
 }
